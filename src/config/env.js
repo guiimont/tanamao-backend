e34718be63a021034aabe8f0b121653edaeb4be6
guiimont <151753574+guiimont@users.noreply.github.com>
2026-03-31 08:49:36 -0300

    Update env.js

--- src/config/env.js
+++ src/config/env.js
@@ -6,8 +6,8 @@ export const env = {
   port: Number(process.env.PORT || 10000),
   nodeEnv: process.env.NODE_ENV || "development",
   frontendUrl: process.env.FRONTEND_URL || "*",
-  frontendSuccessUrl: process.env.FRONTEND_SUCCESS_URL || "https://guiimont.github.io/TanaMao/sucesso.html",
-  frontendFailureUrl: process.env.FRONTEND_FAILURE_URL || "https://guiimont.github.io/TanaMao/falha.html",
+  frontendSuccessUrl: process.env.FRONTEND_SUCCESS_URL || "https://tanamaofit.com.br/TanaMao/sucesso.html",
+  frontendFailureUrl: process.env.FRONTEND_FAILURE_URL || "https://tanamaofit.com.br/TanaMao/falha.html",
   mercadoPagoAccessToken: process.env.MERCADOPAGO_ACCESS_TOKEN || "",
   supabaseUrl: process.env.SUPABASE_URL || "",
   supabaseServiceRoleKey: process.env.SUPABASE_SERVICE_ROLE_KEY || ""
