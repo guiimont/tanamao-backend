2c6dd090dd8c046de649e3f9e37b5513474b8c43
guiimont <151753574+guiimont@users.noreply.github.com>
2026-04-06 11:10:19 -0300

    Update productRoutes.js

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { listProducts } from "../controllers/productsController.js";
 import { deleteProduct } from "../controllers/productsController.js";
+import { createProduct } from "../controllers/productsController.js";
 
 const router = Router();
 router.get("/", listProducts);
 export default router;
 router.delete("/products/:id", deleteProduct);
+router.post("/products", createProduct);
