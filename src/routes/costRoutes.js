ebb208287b3ead6830a4ffc1a2641e080d418b7a
guiimont <151753574+guiimont@users.noreply.github.com>
2026-04-13 18:01:37 -0300

    Update costRoutes.js

--- src/routes/costRoutes.js
+++ src/routes/costRoutes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { listCosts, createCost } from "../controllers/costController.js";
+// Importando a proteção
+import { verifyToken, requireAdmin } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
-router.get("/", listCosts);
-router.post("/", createCost);
+// Protegendo as rotas de custos
+router.get("/", verifyToken, requireAdmin, listCosts);
+router.post("/", verifyToken, requireAdmin, createCost);
 
 export default router;
