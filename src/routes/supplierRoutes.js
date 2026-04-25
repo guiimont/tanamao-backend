28411f0cbe0886c57244af4505fac38579f95f2d
guiimont <151753574+guiimont@users.noreply.github.com>
2026-04-25 13:40:00 -0300

    Update supplierRoutes.js

--- src/routes/supplierRoutes.js
+++ src/routes/supplierRoutes.js
@@ -1,7 +1,12 @@
 import { Router } from "express";
 import { listSuppliers, createSupplier, updateSupplier, deleteSupplier } from "../controllers/supplierController.js";
+import { verifyToken, requireAdmin } from "../middlewares/authMiddleware.js";
 
 const router = Router();
+
+router.use(verifyToken);
+router.use(requireAdmin);
+
 router.get("/", listSuppliers);
 router.post("/", createSupplier);
 router.put("/:id", updateSupplier);
