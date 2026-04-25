12504b9ae0bbc1b7b23dc58cf6e8ec510f368d98
guiimont <151753574+guiimont@users.noreply.github.com>
2026-04-25 13:38:18 -0300

    Update stockRoutes.js

--- src/routes/stockRoutes.js
+++ src/routes/stockRoutes.js
@@ -1,7 +1,12 @@
 import { Router } from "express";
 import { listStock, createStock } from "../controllers/stockController.js";
+import { verifyToken } from "../middlewares/authMiddleware.js";
 
 const router = Router();
+
+// Defesa em profundidade: exige login para qualquer rota de estoque
+router.use(verifyToken);
+
 router.get("/", listStock);
 router.post("/", createStock);
 
