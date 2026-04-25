24b1ba0cf5ce66b231c7c4e91a49493355bbe99a
guiimont <151753574+guiimont@users.noreply.github.com>
2026-04-25 13:40:59 -0300

    Update settingsRoutes.js

--- src/routes/settingsRoutes.js
+++ src/routes/settingsRoutes.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import { getSettings, updateSettings } from "../controllers/settingsController.js";
+import { verifyToken, requireAdmin } from "../middlewares/authMiddleware.js";
 
 const router = Router();
+
+router.use(verifyToken);
+router.use(requireAdmin);
 
 router.get("/:key", getSettings);
 router.put("/:key", updateSettings);
